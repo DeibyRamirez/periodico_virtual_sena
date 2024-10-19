a9eb698417c1adccb13bff30df4c41d127225205
Deiby Ramirez <deibyalejandroramirez13@gmail.com>
2024-10-19 15:55:11 -0500

    Agrego crucigrama al html, estilos y logica del .js

--- crucigrama.js
+++ crucigrama.js
@@ -1,9 +1,34 @@
 // crucigrama.js
-document.addEventListener('DOMContentLoaded', function () {
-    const crossword = [
-        { clue: "1. Capacidad para crear cosas nuevas (Horizontal)", answer: "Innovacion", row: 1, col: 1, direction: "across" },
-        { clue: "2. Actitud responsable frente a las obligaciones (Vertical)", answer: "Responsabilidad", row: 1, col: 3, direction: "down" }
-    ];
+document.getElementById('verificar').addEventListener('click', function () {
+    const inputs = document.querySelectorAll('#crucigrama-juego input');
+    let todasCorrectas = true;
 
-    // Aquí agregas la lógica para construir el crucigrama en la página
+    inputs.forEach(input => {
+        const respuestaCorrecta = input.getAttribute('data-answer');
+        const respuestaUsuario = input.value.toUpperCase();
+
+// Condicional de si las respuestas ingresadas son correctas..
+        if (respuestaUsuario == respuestaCorrecta) {
+            input.style.backgroundColor = '#c8e6c9';
+        } else {
+            input.style.backgroundColor = '#ffccbc';
+            todasCorrectas = false;
+        }
+
+
+    });
+    const resultado = document.getElementById('resultado');
+    
+    //Condicional si todas son correctas felicita y agrega color verde,
+    // si no inteta de nuevo y agrega color rojo...
+
+    if (todasCorrectas) {
+        resultado.textContent = '¡Felicidades! Todas las respuestas son correctas...';
+        resultado.style.color = 'green';
+
+    }
+    else {
+        resultado.textContent = 'Algunas respuestas son correctas.  ¡Intentalo de  nuevo! ';
+        resultado.style.color = 'red';
+    }
 });
